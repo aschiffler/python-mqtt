4d0fea4742b227530cddb1be3198c5febc922c9d
a <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>
2022-08-01 11:49:20 +0000

    updates

--- mycallbacks.py
+++ mycallbacks.py
@@ -2,18 +2,18 @@ from datetime import datetime as dt
 from paho.mqtt.client import connack_string as ack
 
 def on_connect(client, userdata, flags, rc, v5config=None):
-    print(dt.now().strftime("%H:%M:%S.%f")[:-2] + " Connection returned result: "+ack(rc) + " my userdata is " + str(userdata))
+    print(dt.now().strftime("%H:%M:%S.%f")[:-2] + " Connection returned result: "+ack(rc))
 
 def on_message(client, userdata, message,tmp=None):
     print(dt.now().strftime("%H:%M:%S.%f")[:-2] + " Received message " + str(message.payload) + " on topic '"
-        + message.topic + "' with QoS " + str(message.qos) + " my userdata is " + str(userdata))
+        + message.topic + "' with QoS " + str(message.qos))
 
 def on_publish(client, userdata, mid,tmp=None):
-    print(dt.now().strftime("%H:%M:%S.%f")[:-2] + " Published message id: "+str(mid) + " my userdata is " + str(userdata))
+    print(dt.now().strftime("%H:%M:%S.%f")[:-2] + " Published message id: "+str(mid))
     
 def on_subscribe(client, userdata, mid, qos,tmp=None):
     if isinstance(qos, list):
         qos_msg = str(qos[0])
     else:
         qos_msg = f"and granted QoS {qos[0]}"
-    print(dt.now().strftime("%H:%M:%S.%f")[:-2] + " Subscribed " + qos_msg + " my userdata is " + str(userdata))    
+    print(dt.now().strftime("%H:%M:%S.%f")[:-2] + " Subscribed " + qos_msg)    
